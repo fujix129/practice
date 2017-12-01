09cea0422822572e1f56cd9eb7d036eb10d981b6
fujix129 <minami_hyoufk@yahoo.co.jp>
2017-12-01 16:34:54 +0900

    祝日お知らせ機能追加

--- bot/index.js
+++ bot/index.js
@@ -9,24 +9,54 @@ const controller = Botkit.slackbot({
     debug: false
 });
 
+//controller.spawn({
+//    token: process.env.token
+//}).startRTM(function(err){
+//    if (err) {
+//        throw new Error(err);
+//    }
+//});
+
+const cron = require('cron');
+//const controller = Botkit.slackbot();
 controller.spawn({
-    token: process.env.token
-}).startRTM(function(err){
-    if (err) {
-        throw new Error(err);
-    }
+    token : process.env.token
+}).startRTM((err, bot, payload) => {
+    new cron.CronJob({
+        cronTime: '00 00 18 * * 0-4',
+        onTick: () => {
+        var now = new Date();
+        var tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+        var holiday = JapaneseHolidays.isHoliday(tomorrow);
+          if(holiday) {
+            bot.say({
+              channel: 'bot_test',
+              text: "明日は " + holiday + " でお休みだよ！やったね！"
+            });
+          } else {
+             bot.say({
+                channel: 'bot_test',
+                text:"明日はお仕事。頑張って働こう。"
+           });
+        }
+
+        },
+        start: true,
+        timeZone: 'Asia/Tokyo'
+    });
 });
 
 var JapaneseHolidays = require('japanese-holidays');
 
-controller.hears('今日は',['direct_message','direct_mention','mention'],function(bot,message) {
+controller.hears('明日は',['direct_message','direct_mention','mention'],function(bot,message) {
 
-  var today = new Date();
-  var holiday = JapaneseHolidays.isHoliday(today);
+  var now = new Date(2017,11,31);
+  var tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+  var holiday = JapaneseHolidays.isHoliday(tomorrow);
   if(holiday) {
-      bot.reply(message,"今日は " + holiday + " でお休みだよ！やったね！");
+      bot.reply(message,"明日は " + holiday + " でお休みだよ！やったね！");
   } else {
-      bot.reply(message,"今日は祝日ではないんだ。働こう。");
+      bot.reply(message,"明日は "+ tomorrow + "で祝日ではないんだ。働こう。");
   }
 });
 
